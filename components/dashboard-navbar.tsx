ea5949bac3f9ea8d428d491d63ff739edf3120fd
Sara Öhman <saraohman97@gmail.com>
2024-01-28 15:34:48 +0100

    Prettier dashboard navbar.

--- components/dashboard-navbar.tsx
+++ components/dashboard-navbar.tsx
@@ -1,46 +1,53 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { CgWebsite } from "react-icons/cg";
 import { CiInboxIn, CiPen } from "react-icons/ci";
+import { IconBase } from "react-icons/lib";
 
 const DashboardNavbar = () => {
-  const [active, setActive] = useState("blog");
+  // const [active, setActive] = useState("blog");
+  const pathname = usePathname()
+
+  const routes = [
+    {
+      href: '/dashboard',
+      label: 'Blogg',
+      active: pathname === '/dashboard',
+      icon: CiPen
+    },
+    {
+      href: '/dashboard/kategorier',
+      label: 'Kategorier',
+      active: pathname === '/dashboard/kategorier',
+      icon: CiInboxIn
+    },
+    {
+      href: '/dashboard/projekt',
+      label: 'Projekt',
+      active: pathname === '/dashboard/projekt',
+      icon: CgWebsite
+    },
+  ]
 
   return (
     <>
       <div className="flex items-center justify-center gap-4">
+        {routes.map((route) => (
         <Link
-          href="/dashboard"
-          onClick={() => setActive("blog")}
+        key={route.label}
+          href={route.href}
           className={`flex flex-col items-center gap-2 p-4 text-center text-slate-500 hover:text-slate-800 border-b-2 border-transparent hover:border-slate-800 ${
-            active === "blog" && "text-slate-800 border-b-slate-800"
+            route.active && "text-slate-800 border-b-slate-800"
           }`}
         >
-          <CiPen size={25} />
-          <span>Blogg</span>
+          <route.icon size={25} />
+          <span>{route.label}</span>
         </Link>
-        <Link
-          href="/dashboard/kategorier"
-          onClick={() => setActive("category")}
-          className={`flex flex-col items-center gap-2 p-4 text-center text-slate-500 hover:text-slate-800 border-b-2 border-transparent hover:border-slate-800 ${
-            active === "category" && "text-slate-800 border-b-slate-800"
-          }`}
-        >
-          <CiInboxIn size={25} />
-          <span>Kategorier</span>
-        </Link>
-        <Link
-          href="/dashboard/projekt"
-          onClick={() => setActive("project")}
-          className={`flex flex-col items-center gap-2 p-4 text-center text-slate-500 hover:text-slate-800 border-b-2 border-transparent hover:border-slate-800 ${
-            active === "project" && "text-slate-800 border-b-slate-800"
-          }`}
-        >
-          <CgWebsite size={25} />
-          <span>Projekt</span>
-        </Link>
+        ))}
+        
       </div>
 
       <hr className="mb-20" />
