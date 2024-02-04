bbcc1b692933ec360ebb19e33a9802719179c513
Sara Öhman <saraohman97@gmail.com>
2024-02-04 20:48:27 +0100

    d

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -196,7 +196,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
