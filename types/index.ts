a7e0ef804ed5b326049caa2411f645ee65dec745
Sara Öhman <saraohman97@gmail.com>
2024-02-04 18:17:41 +0100

    d

--- types/index.ts
+++ types/index.ts
@@ -9,42 +9,4 @@ export type SafeUser = Omit<
     emailVerified: string | null;
 };
 
-export interface Post {
-    id: string;
-    title: string;
-    description: string;
-    imageDescription: string;
-    preText: string;
-    text: string;
-    favorite: boolean;
-    images: Image[];
-    createdAt: Date;
 
-    NextJS: boolean;
-    ReactJS: boolean;
-    VanillaJS: boolean;
-    MongoDB: boolean;
-    MySQL: boolean;
-    Prisma: boolean;
-    Mongoose: boolean;
-    Shadcn: boolean;
-    Tailwind: boolean;
-    VanillaCSS: boolean;
-}
-export interface Image {
-    id: string;
-    url: string;
-}
-
-export interface Project {
-    id: string;
-    title: string;
-    url: string;
-    images: ProjectImage[]
-}
-
-export interface ProjectImage {
-    id: string;
-    title: string;
-    images: ProjectImage[]
-}
